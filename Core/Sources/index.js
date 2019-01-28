89d96bc38019913b666cb91f5408d2012ac0e481
Jared Mathews <refectjam@gmail.com>
2019-01-27 21:46:15 -0700

    Setup api channel in core

--- Core/Sources/index.js
+++ Core/Sources/index.js
@@ -1,1 +1,61 @@
-console.log('Hello')
+const Http = require('http')
+const createSagaCore = require('create-saga-core')
+const { spawn, call, take } = require('redux-saga/effects')
+const { eventChannel, buffers } = require('redux-saga')
+
+createSagaCore({ initializer })
+
+function* initializer() {
+  yield spawn(apiProcessor)
+}
+
+function* apiProcessor() {
+  const { apiChannel } = yield call(createApiChannel)
+  while (true) {
+    const { apiRequest } = yield take(apiChannel)
+    console.log(apiRequest.type)
+    console.log(apiRequest.payload)
+  }
+}
+
+function createApiChannel() {
+  return new Promise(resolve => {
+    const apiServer = Http.createServer()
+    const apiChannel = eventChannel(emit => {
+      apiServer.on('request', (httpRequest, httpResponse) => {
+        const validApiRequest =
+          httpRequest.url === '/api' && httpRequest.method === 'POST'
+        if (validApiRequest) {
+          parseApiRequestBody({
+            apiRequest: httpRequest
+          }).then(({ apiRequestBody }) => {
+            const apiRequest = {
+              ...apiRequestBody,
+              responder: httpResponse
+            }
+            emit({ apiRequest })
+          })
+        } else {
+          httpResponse.statusCode = 400
+          httpResponse.end()
+        }
+      })
+      return () => null
+    }, buffers.expanding())
+    apiServer.listen(3000, () => resolve({ apiChannel }))
+  })
+}
+
+function parseApiRequestBody({ apiRequest }) {
+  return new Promise(resolve => {
+    let requestData = []
+    apiRequest
+      .on('data', requestChunk => {
+        requestData.push(requestChunk)
+      })
+      .on('end', () => {
+        let apiRequestBody = JSON.parse(requestData)
+        resolve({ apiRequestBody })
+      })
+  })
+}
